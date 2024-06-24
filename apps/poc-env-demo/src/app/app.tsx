daf32511c462586003fc5e34189735c9b60dcc0a
Grant Walker <grant.walker@ksdc.co.za>
2024-06-24 10:01:23 +0200

    added env files

--- apps/poc-env-demo/src/app/app.tsx
+++ apps/poc-env-demo/src/app/app.tsx
@@ -6,6 +6,8 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <div>
+      <p>This is a VITE Env Var: {import.meta.env.VITE_ENV_VAR}</p>
+      <p>This is a NX Env Var: {import.meta.env.NX_ENV_VAR}</p>
       <NxWelcome title="poc-env-demo" />
     </div>
   );
